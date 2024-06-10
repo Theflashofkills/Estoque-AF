ca50f9124d076c0753a87e466a9d6b19e2b35bc9
leoori <alemaoproenca@hotmail.com>
2024-06-10 20:50:22 -0300

    interface

--- Estoque.py
+++ Estoque.py
@@ -1,32 +1,4 @@
-from ArvoreBinaria import ArvoreBinaria
 from Produto import Produto
-
-
-def menu():
-    estoque = ArvoreBinaria()
-    while True:
-        print("\n1. Cadastrar Produto")
-        print("2. Remover Produto")
-        print("3. Atualizar Produto")
-        print("4. Consultar Produto")
-        print("5. Relatório de Estoque")
-        print("6. Sair")
-        escolha = input("Escolha uma opção: ")
-
-        if escolha == '1':
-            cadastrar_produto(estoque)
-        elif escolha == '2':
-            remover_produto(estoque)
-        elif escolha == '3':
-            atualizar_produto(estoque)
-        elif escolha == '4':
-            consultar_produto(estoque)
-        elif escolha == '5':
-            relatorio_estoque(estoque)
-        elif escolha == '6':
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
 
 def cadastrar_produto(estoque):
     id = int(input("ID: "))
@@ -125,5 +97,3 @@ def relatorio_estoque(estoque):
         else:
             print("Opção inválida. Tente novamente.")
 
-if __name__ == "__main__":
-    menu()
